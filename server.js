02bdeea174e2e115b996e8fce7dfd488f14dd031
frostsalazar22 <frostsalazar@hotmail.com>
2024-10-28 20:03:27 -0300

    Upgrade
    Atualização do sistema

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+//Importando o Framework
 const express = require('express');
+
+//cookies e sessions
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+
+//Iniciar o express
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session({
-  secret: 'seuSegredoAqui',
+  secret: 'Segredosecreto',
   resave: false,
   saveUninitialized: true
 }));
@@ -17,37 +22,37 @@ app.listen(3000, () => {
 
 // Página inicial
 app.get('/', (req, res) => {
-  res.send('<h1>Bem-vindo! <a href="/login">Login</a> | <a href="/protected">Página Protegida</a></h1>');
+  res.send('<h1>Bem-vindo <a href="/login">Login</a> | <a href="/seguro">Usuário Cadastrado[Entrar]</a></h1>');
 });
 
 // Página de login
 app.get('/login', (req, res) => {
-  res.send('<form method="POST" action="/login">Usuário: <input type="text" name="username"/><br>Senha: <input type="password" name="password"/><br><button type="submit">Login</button></form>');
+  res.send('<form method="POST" action="/login">Usuário: <input type="text" name="username"/><br>Senha: <input type="password" name="password"/><br><button type="submit">Login</button></form> <br><a>Usuário:giovane</br></a><br><a>Senha:senha</br></a>');
 });
 
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
   
-  // Validação simples
-  if (username === 'usuario' && password === 'senha') {
+  // Validação
+  if (username === 'giovane' && password === 'senha') {
     req.session.user = username;
-    res.redirect('/protected');
+    res.redirect('/seguro');
   } else {
-    res.send('Login falhou. <a href="/login">Tente novamente</a>');
+    res.send('Usuário não Cadastrado, <a href="/login">Tente novamente</a>');
   }
 });
 
 app.get('/logout', (req, res) => {
   req.session.destroy();
-  res.send('Você saiu! <a href="/">Voltar à página inicial</a>');
+  res.send('Você Deslogou da Sua Conta <a href="/">Voltar à página inicial</a>');
 });
 
-app.get('/protected', (req, res) => {
+app.get('/seguro', (req, res) => {
   if (req.session.user) {
-    res.send(`<h1>Página Protegida</h1><p>Bem-vindo, ${req.session.user}! <a href="/logout">Logout</a></p>`);
+    res.send(`<h1>Página Do Usuário Logado</h1><p>Bem-vindo, ${req.session.user}! </p> <p> <a href="/logout">Logout</a> ,ou, <a href="/">Retornar Ao Inicio</a> </p>`);
   } else {
-    res.send('Acesso negado! <a href="/login">Faça login</a>');
+    res.send('Acesso negado <a href="/login">Faça login para continuar</a>');
   }
 });
 
@@ -60,6 +65,6 @@ function authMiddleware(req, res, next) {
 }
 
 // Aplicar o middleware na rota protegida
-app.get('/protected', authMiddleware, (req, res) => {
-  res.send(`<h1>Página Protegida</h1><p>Bem-vindo, ${req.session.user}! <a href="/logout">Logout</a></p>`);
+app.get('/seguro', authMiddleware, (req, res) => {
+  res.send(`<h1>Página Do Usuário Logado</h1><p>Bem-vindo, ${req.session.user}! </p> <p> <a href="/logout">Logout</a> ,ou, <a href="/">Retornar Ao Inicio</a> </p>`);
 });
